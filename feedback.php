8114a6cba36bed39db2220086fc21dc805f5a583
nfactoryair <ffffactoryy@gmail.com>
2016-10-29 15:02:18 +0800

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -1,17 +1,19 @@
 <?php
-
+if (isset($_POST["submit"])) {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message']; 
 
 $to = "ffffactoryy@gmail.com";
 $subject = "site message";
-$body = "此為系統發信，請勿回覆";
+$body = "此為系統發信，請勿回覆" .$message;
 
 mail ($to,$subject,$body);
 
-echo "訊息已送出！\n\n $request";
-
+if(mail($to,$subject,$body)) {
+echo "郵件已寄出!\n";
+} else {
+echo "郵件寄送失敗!\n";}}
 ?>
 
 
